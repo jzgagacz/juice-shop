0f8ba1e0032d27f3965efafc112c2b486dd391b0
jzgagacz <01142603@pw.edu.pl>
2022-11-14 15:45:19 +0100

    Fix marsdb RCE

--- routes/showProductReviews.ts
+++ routes/showProductReviews.ts
@@ -27,11 +27,11 @@ global.sleep = (time: number) => {
 
 module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const id = utils.disableOnContainerEnv() ? Number(req.params.id) : req.params.id
+    const id = Number(req.params.id)
 
     // Measure how long the query takes, to check if there was a nosql dos attack
     const t0 = new Date().getTime()
-    db.reviews.find({ $where: 'this.product == ' + id }).then((reviews: Review[]) => {
+    db.reviews.find({ product: id }).then((reviews: Review[]) => {
       const t1 = new Date().getTime()
       challengeUtils.solveIf(challenges.noSqlCommandChallenge, () => { return (t1 - t0) > 2000 })
       const user = security.authenticatedUsers.from(req)
